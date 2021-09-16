2f4ba505ad2cdaea557f03077ccb8222f86b1c1e
fabriceL <fabrice.latri@formasup-paris.com>
2021-09-16 09:36:04 +0200

    correction

--- www/public/index.php
+++ www/public/index.php
@@ -2,10 +2,9 @@
 
 require "../vendor/autoload.php";
 
-
 $app = new \Framework\App([
   BlogModule::class
 ]);
 
 $response = $app->run(\GuzzleHttp\Psr7\ServerRequest::fromGlobals());
-Http\Response\send($response);
+\Http\Response\send($response);
